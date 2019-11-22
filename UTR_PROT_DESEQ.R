ebbc2a2b1ec7db907fa401428d6f521eaf0b71a9
florekem <maciej.florczyk.ml@gmail.com>
2019-11-22 16:13:41 +0100

    koleje losu

--- UTR_PROT_DESEQ.R
+++ UTR_PROT_DESEQ.R
@@ -1,18 +1,58 @@
 library(DESeq2)
+### PBS vs MCLR, stosunki PROT/UTR
+#data <- read.csv('/mnt/sdb1/Project_Sieja_MCLR_52/2_pipeline/7_counting_transcripts/noncoding_novel/day_1_UTR_PROT_matryca.csv', sep=',', head=TRUE);
+#coldata <- as.matrix(read.table('/mnt/sdb1/Project_Sieja_MCLR_52/2_pipeline/7_counting_transcripts/noncoding_novel/coldata_1d_stosunki.csv'));
+#dds<-DESeqDataSetFromMatrix(countData=data,colData=coldata,design = ~condition+position+condition:position);
+#dds<-DESeq(dds);
+#res<-results(dds,name="conditionuntreated.positionB");
+#write.csv(res, file="/mnt/sdb1/Project_Sieja_MCLR_52/2_pipeline/7_counting_transcripts/noncoding_novel/deseq2_result_from_R_1d_2.csv", row.names=FALSE);
 
-#data <- read.csv('/mnt/sdb1/Project_Sieja_MCLR_52/2_pipeline/7_counting_transcripts/noncoding_novel/day_1_UTR_PROT_matryca.csv', sep=',', head=TRUE)
-data <- read.csv('/mnt/sdb1/Project_Sieja_MCLR_52/2_pipeline/7_counting_transcripts/noncoding_novel/PBS1d_MC_all_matryca_1d_do_deseq_bez_trinity.csv', sep=',', head=TRUE)
 
-coldata <- as.matrix(read.table('/mnt/sdb1/Project_Sieja_MCLR_52/2_pipeline/7_counting_transcripts/noncoding_novel/coldata_1d.csv'))
+### nowe UTR/PROT oddzielnie
+##UTR
+#1d
+#data <- read.csv('/mnt/sdb1/Project_Sieja_MCLR_52/2_pipeline/7_counting_transcripts/noncoding_novel/PBS1d_MC_all_matryca_1d_do_deseq_bez_trinity.csv', sep=',', head=TRUE)
+#6d
+#data <- read.csv('/mnt/sdb1/Project_Sieja_MCLR_52/2_pipeline/7_counting_transcripts/noncoding_novel/PBS1d_MC_all_matryca_6d_do_deseq_bez_trinity.csv', sep=',', head=TRUE)
+#9d
+#data <- read.csv('/mnt/sdb1/Project_Sieja_MCLR_52/2_pipeline/7_counting_transcripts/noncoding_novel/PBS1d_MC_all_matryca_9d_do_deseq_bez_trinity.csv', sep=',', head=TRUE)
 
-dds <- DESeqDataSetFromMatrix(countData=data,colData=coldata,design = ~ condition);
+##PROT
+#1d
+#data <- read.csv('/mnt/sdb1/Project_Sieja_MCLR_52/2_pipeline/7_counting_transcripts/proteins_salmo/PBS_MCLR_salmo_only_1d_matryca_do_deseq_bez_trinity.csv', sep=',', head=TRUE)
+#6d
+#data <- read.csv('/mnt/sdb1/Project_Sieja_MCLR_52/2_pipeline/7_counting_transcripts/proteins_salmo/PBS_MCLR_salmo_only_6d_matryca_do_deseq_bez_trinity.csv', sep=',', head=TRUE)
+#9d
+#data <- read.csv('/mnt/sdb1/Project_Sieja_MCLR_52/2_pipeline/7_counting_transcripts/proteins_salmo/PBS_MCLR_salmo_only_9d_matryca_do_deseq_bez_trinity.csv', sep=',', head=TRUE)
 
+
+
+#coldata <- as.matrix(read.table('/mnt/sdb1/Project_Sieja_MCLR_52/2_pipeline/7_counting_transcripts/noncoding_novel/coldata_9d.csv'))
+
+#dds <- DESeqDataSetFromMatrix(countData=data,colData=coldata,design = ~ condition);
+#dds <- DESeq(dds);
+#wyniki<-results(dds);
+#wyniki_005 <- results(dds,name="conditionPBS.positionB", alpha=0.05);
+#summary(wyniki)
+
+##UTR
+#
+#write.csv(wyniki, file="/mnt/sdb1/Project_Sieja_MCLR_52/2_pipeline/7_counting_transcripts/noncoding_novel/NOVEL_deseq2_result_from_R_9d.csv", row.names=FALSE)
+ 
+
+##PROT
+#1d
+#write.csv(wyniki, file="/mnt/sdb1/Project_Sieja_MCLR_52/2_pipeline/7_counting_transcripts/proteins_salmo/PROT_deseq2_result_from_R_9d.csv", row.names=FALSE)
+
+
+### PBS_UTR vs PBS_PROT
+data <- read.csv('/mnt/sdb1/Project_Sieja_MCLR_52/2_pipeline/7_counting_transcripts/noncoding_novel/day_1_PBS_UTR_PROT_matryca.csv', sep=',', head=TRUE)
+coldata <- as.matrix(read.table('/mnt/sdb1/Project_Sieja_MCLR_52/2_pipeline/7_counting_transcripts/noncoding_novel/coldata_1d_PBS.csv'))
+
+dds <- DESeqDataSetFromMatrix(countData=data,colData=coldata,design = ~ position);
 dds <- DESeq(dds);
-
 wyniki<-results(dds);
-wyniki_005 <- results(dds,name="conditionPBS.positionB", alpha=0.05);
+write.csv(wyniki, file="/mnt/sdb1/Project_Sieja_MCLR_52/2_pipeline/7_counting_transcripts/noncoding_novel/deseq2_result_from_R_1d_PBS_UTR_PBS_PROT.csv", row.names=FALSE);
 
-summary(wyniki)
 
-write.csv(wyniki, file="/mnt/sdb1/Project_Sieja_MCLR_52/2_pipeline/7_counting_transcripts/noncoding_novel/NOVEL_deseq2_result_from_R_1d.csv", row.names=FALSE)
-  
+    
